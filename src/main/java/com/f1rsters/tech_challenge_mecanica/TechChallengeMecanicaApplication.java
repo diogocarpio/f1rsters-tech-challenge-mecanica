52293570a4a90c49294ee7428abaa1fa9da15e48
Diogo Felix <diogo.felix92@hotmail.com>
2026-04-03 19:52:07 -0300

    Criando a estrtutura do projeto

---
+++ src/main/java/com/f1rsters/tech_challenge_mecanica/TechChallengeMecanicaApplication.java
@@ -0,0 +1,13 @@
+package com.f1rsters.tech_challenge_mecanica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeMecanicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeMecanicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f1rsters/tech_challenge_mecanica/TechChallengeMecanicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1rsters.tech_challenge_mecanica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeMecanicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
